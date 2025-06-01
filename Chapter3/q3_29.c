6fe1d347c8ae7b6a45f9b68e0b9556c8ac0d42e3
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-06-01 09:11:18 +0200

    Create q3_29.c

---
+++ Chapter3/q3_29.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+What does the following program print?
+*/
+
+int main(void) {
+
+    int row = 10; // initialize row
+    while (row >= 1) { // loop until row < 1
+        int column = 1; // set column to 1 as iteration begins
+        while (column <= 10) { // loop 10 times
+            printf("%s", (row % 2) ? "<": ">"); // output
+            ++column; // increment column
+        } // end inner while
+        --row; // decrement row
+        puts(""); // begin new output line
+    } // end outer while
+
+    return 0;
+}
